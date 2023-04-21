bdceecc7856b6f1a9ba9fc91cf66f5c6dcaa07d2
tanishghosh <tanishghosh@gmail.com>
2023-04-21 23:18:51 +0530

    moved API fetch to useEffect hook

--- src/searchbar/Searchbar.jsx
+++ src/searchbar/Searchbar.jsx
@@ -2,35 +2,40 @@ import React from "react";
 import { useState, useEffect } from "react";
 import constants from '../constants.json'
 
-export default function Searchbar(){
+export default function Searchbar() {
 
     let [searchterm, setSearchTerm] = useState("");
     let [searchResults, setSearchResults] = useState([]);
 
-    return(
+    useEffect(() => {
+        if (searchterm === '')
+            setSearchResults([])
+        else {
+            fetch('https://api.themoviedb.org/3/search/tv?query=' + searchterm + '&api_key=' + constants["api-key"])
+                .then(response => response.json())
+                .then(json => {
+                    setSearchResults(json["results"])
+                });
+        }
+    }, [searchterm])
+
+    return (
         <div>
-            <input type="text" name="search-bar" id="search-bar" onInput={setSearchTermOnInput}/>
+            <input type="text" name="search-bar" id="search-bar" onInput={setSearchTermOnInput} />
             <h2>{searchterm}</h2>
             <ul>
                 {
-                searchResults.length>0 && searchResults.map((searchResult, i) => {
-                            console.log(searchResult);
-                            return <li key={i}>{searchResult.original_name}</li>
+                    searchResults.length > 0 && searchResults.map((searchResult, i) => {
+                        return <li key={i}>{searchResult.original_name}</li>
                     })
                 }
             </ul>
         </div>
     )
 
-    function setSearchTermOnInput(){
+    function setSearchTermOnInput() {
         let x = document.getElementById("search-bar").value;
-        setSearchTerm(x);
-
-        fetch('https://api.themoviedb.org/3/search/tv?query='+searchterm+'&api_key='+ constants["api-key"])
-        .then(response => response.json())
-        .then(json => {
-            setSearchResults(json["results"])
-        });
+        setSearchTerm(x)
     }
 
 }
